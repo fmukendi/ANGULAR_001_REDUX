a2ca26d94df1dbc3a8d2af33dc34474c11f36ca3
Franck Mukendi <franck.mukendi@gmail.com>
2021-03-21 17:46:50 -0400

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgRedux } from "ng2-redux";
+import { NgRedux, select } from "ng2-redux";
 import { Component } from "@angular/core";
 
 import { IAppState, rootReducer } from "./store";
@@ -11,9 +11,23 @@ import { INCREMENT } from "./action";
 })
 export class AppComponent {
   title = "app works!";
-  counter = 0;
+  // counter = 0;
+  // @select() counter;
+  @select('counter') count;
+  @select(['messaging', 'newMessages']) newMessages;
+  // messaging.newMessages
+  @select((s: IAppState)=> s.messaging.newMessages) newMessagesCount;
 
-  constructor(private ngRedux: NgRedux<IAppState>) {}
+  constructor(private ngRedux: NgRedux<IAppState>) {
+    // problem below is that you'll have to unsubscribe
+    // no need if you use an async pipe with,
+    // and with select, no need for code below
+    // var subscription = ngRedux.subscribe(()=> {
+    //    // console.log(ngRedux.getState());
+    //    var store = ngRedux.getState();
+    //    this.counter = store.counter;
+    // })
+  }
 
   increment() {
     // this.counter++;
