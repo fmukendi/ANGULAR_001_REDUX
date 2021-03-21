5373522e473b879b150a68ce7116e7688b5466d7
Franck Mukendi <franck.mukendi@gmail.com>
2021-03-21 18:07:14 -0400

    push

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,28 @@
 import { INCREMENT } from "./action";
+import { tassign } from "tassign";
 
 export interface IAppState {
-    counter: number;
-    messaging?: {
-        newMessages: number
-    }
+  counter: number;
+  messaging?: {
+    newMessages: number;
+  };
 }
 
 export const INITIAL_STATE: IAppState = {
-    counter: 0,
-    messaging: {
-        newMessages: 5
-    }
-}
+  counter: 0,
+  messaging: {
+    newMessages: 5,
+  },
+};
 
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
     case INCREMENT:
-        return { counter: state.counter + 1};
+    // return { counter: state.counter + 1};
+    // return Object.assign({}, state, { counter: state.counter + 1})
+    // return Object.assign({}, state, { counter: state.counter + 1, bugProp: 1}) // bad !!!
+    // state.counter++; // bad !!! Do not change state as well
+    return tassign( state, { counter: state.counter + 1}); // tassign protect proprety
 
     default:
       break;
